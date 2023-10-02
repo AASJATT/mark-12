b034b60a1f26fb19bc592eb28983c38e4dc89c8c
AASJATT <141618407+AASJATT@users.noreply.github.com>
2023-10-02 16:03:43 +0000

    docs: html&js updated

---
+++ quiz.js
@@ -0,0 +1,5 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitAnswerBtn = document.querySelector("#submit-answer-btn");
+const outputDiv = document.querySelector("#output");
+
+const correctAnswers = ["90","right angle"];
