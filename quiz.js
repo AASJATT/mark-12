372d2ee3bc2915972fce7bd3986a79ed20d85e52
AASJATT <141618407+AASJATT@users.noreply.github.com>
2023-10-04 15:04:59 +0000

    docs: function updated

--- quiz.js
+++ quiz.js
@@ -3,3 +3,18 @@ const submitAnswerBtn = document.querySelector("#submit-answer-btn");
 const outputDiv = document.querySelector("#output");
 
 const correctAnswers = ["90","right angle"];
+
+function calculateScore(){
+    let score=0;
+    let index=0;
+    const formResults = new FormData(quizForm);
+    for(let value of formResults.values()){
+        if(value=== correctAnswers[index]){
+            score = score+1;
+        }
+        index=index+1;
+    }
+    outputDiv.innerText= "Your score is" + score; 
+}
+
+submitAnswerBtn.addEventListener('click', calculateScore);
